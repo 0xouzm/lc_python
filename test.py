470b6cd4c24a1c8b6563761bb09467edc982608c
wujj <wujj@cpchain.io>
2019-12-13 20:28:45 +0800

    ls init

---
+++ test.py
@@ -0,0 +1,44 @@
+from typing import List
+from itertools import *
+import math
+
+
+class Solution:
+    def judgePoint24(self, nums: List[int]) -> bool:
+        operands = self.get_operands(nums)
+        ops = self.get_oprators()
+        for num in operands:
+            for op in ops:
+                s = zip_longest(num, op, fillvalue='')
+                res_s = chain.from_iterable(s)
+                calculate_str = ''
+                for i in res_s:
+                    calculate_str += i
+                s = calculate_str
+                formats = [
+                    '(({}{}{}){}{}){}{}', '({}{}({}{}{})){}{}',
+                    '{}{}(({}{}{}){}{})', '{}{}({}{}({}{}{}))',
+                    '({}{}{}){}({}{}{})'
+                ]
+                for f in formats:
+                    check_str = f.format(s[0], s[1], s[2], s[3], s[4], s[5],
+                                         s[6])
+                    print(check_str)
+                    try:
+                        if math.isclose(eval(check_str), 24):
+                            return True
+                    except ZeroDivisionError:
+                        continue
+        return False
+
+    def get_operands(self, nums):
+        oprands = []
+        for i in permutations([str(i) for i in nums]):
+            oprands.append(i)
+        return oprands
+
+    def get_oprators(self):
+        ops = []
+        for op in product('+-*/', repeat=3):
+            ops.append(op)
+        return ops
