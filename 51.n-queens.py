8bb26cb738d3b1dac3ff0c28b7387d1e0fde2228
wujj <wujj@cpchain.io>
2019-12-15 17:40:15 +0800

    n queens I

--- 51.n-queens.py
+++ 51.n-queens.py
@@ -7,16 +7,19 @@
 # @lc code=start
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        def DFS(queens, xy_dif, xy_sum):
+        def DFS(queens, xy_diff, xy_sum):
             p = len(queens)
-            if p==n:
+            if p == n:
                 result.append(queens)
-                return None
+                return
             for q in range(n):
-                if q not in queens and p-q not in xy_dif and p+q not in xy_sum: 
-                    DFS(queens+[q], xy_dif+[p-q], xy_sum+[p+q])  
+                if q not in queens and p - q not in xy_diff and p + q not in xy_sum:
+                    DFS(queens + [q], xy_diff + [p - q], xy_sum + [p + q])
+
         result = []
-        DFS([],[],[])
-        return [ ["."*i + "Q" + "."*(n-i-1) for i in sol] for sol in result]
+        DFS([], [], [])
+        return [["." * i + "Q" + "." * (n - i - 1) for i in sol] for sol in result]
+
+
 # @lc code=end
 
