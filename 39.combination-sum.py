f828924acdbb2955f5e4575c0c390a386b470a23
wujj <wujj@cpchain.io>
2019-12-29 19:28:08 +0800

    combination and permutation

--- 39.combination-sum.py
+++ 39.combination-sum.py
@@ -16,31 +16,31 @@
 # Given a set of candidate numbers (candidates) (without duplicates) and a
 # target number (target), find all unique combinations in candidates where the
 # candidate numbers sums to target.
-# 
+#
 # The same repeated number may be chosen from candidates unlimited number of
 # times.
-# 
+#
 # Note:
-# 
-# 
+#
+#
 # All numbers (including target) will be positive integers.
 # The solution set must not contain duplicate combinations.
-# 
-# 
+#
+#
 # Example 1:
-# 
-# 
+#
+#
 # Input: candidates = [2,3,6,7], target = 7,
 # A solution set is:
 # [
 # ⁠ [7],
 # ⁠ [2,2,3]
 # ]
-# 
-# 
+#
+#
 # Example 2:
-# 
-# 
+#
+#
 # Input: candidates = [2,3,5], target = 8,
 # A solution set is:
 # [
@@ -48,13 +48,26 @@
 # [2,3,3],
 # [3,5]
 # ]
-# 
-# 
+#
+#
 #
 
 # @lc code=start
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        
+        ans = []
+        cur = []
+
+        def dfs(candidates, target, l, cur, ans):
+            if l == len(candidates):
+                if sum(cur) == 7:
+                    ans.append(cur)
+                    return
+            for i in candidates:
+                if sum(cur) + i <= target:
+                    dfs(candidates, target, l, cur, ans)
+                dfs(candidates, target, l + 1, cur, ans)
+
+
 # @lc code=end
 
--- test.py
+++ test.py
@@ -1,26 +1,65 @@
-from typing import List
-
-class Solution:
-    def letterCombinations(self, digits: str) -> List[str]:
-        if not digits:
-            return []
-        d = [" ", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"]
-
-        def dfs(digits, d, l, cur, ans):
-            if l == len(digits):
-                ans.append("".join(cur))
-                return
-            c = d[int(digits[l])]
-            cur[l] = c
-            dfs(digits, d, l + 1, cur, ans)
-
-        ans = []
-        cur = ["" for _ in range(len(digits))]
-        dfs(digits, d, 0, cur, ans)
-        return ans
+# from typing import List
 
 
-digits = "23"
-s = Solution()
+# class Solution:
+#     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+#         ans = []
+#         cur = []
 
-print(s.letterCombinations(digits))
+#         def dfs(candidates, target, l, cur, ans):
+#             if l == len(candidates):
+#                 if sum(cur) == 7:
+#                     ans.append(cur)
+#                     return
+#             if sum(cur) == 7:
+#                 ans.append(cur)
+#                 return
+
+#             for i in candidates:
+#                 if sum(cur) + i <= target:
+#                     cur.append(candidates[l])
+#                     dfs(candidates, target, l, cur, ans)
+#                 dfs(candidates, target, l + 1, cur, ans)
+
+#         dfs(candidates, target, 0, cur, ans)
+#         return ans
+
+
+# s = Solution()
+# print(s.combinationSum([2, 3, 6, 7], 7))
+
+
+
+def permutations(nums, d, n, used, cur, ans):
+    if d == n:
+        ans.append(cur[:])
+        return
+    for i in range(0, len(nums)):
+        if used[i]:
+            continue
+        used[i] = True
+        cur.append(nums[i])
+        permutations(nums, d + 1, n, used, cur, ans)
+        cur.pop()
+        used[i] = False
+
+
+def combination(nums, d, n, s, cur, ans):
+    if d == n:
+        ans.append(cur[:])
+        return
+    for i in range(s, len(nums)):
+        cur.append(nums[i])
+        combination(nums, d + 1, n, i + 1, cur, ans)
+        cur.pop()
+
+
+ans = []
+permutations([1, 2, 3], 0, 3, [False] * 3, [], ans)
+print(ans)
+
+c = []
+
+combination([1, 2, 3], 0, 3, 0, [], c)
+print(c)
+
