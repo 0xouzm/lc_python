358fcaea75002be1ee01fe5a3e1717dd766e3d95
wujj <wujj@cpchain.io>
2019-12-30 14:17:12 +0800

    40

--- 40.combination-sum-ii.py
+++ 40.combination-sum-ii.py
@@ -1,30 +1,10 @@
-from typing import List
+#
+# @lc app=leetcode id=40 lang=python3
+#
+# [40] Combination Sum II
+#
 
-
-# class Solution:
-#     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-#         ans = []
-#         cur = []
-
-#         def dfs(candidates, target, l, cur, ans):
-#             if l == len(candidates):
-#                 if sum(cur) == 7:
-#                     ans.append(cur)
-#                     return
-#             if sum(cur) == 7:
-#                 ans.append(cur)
-#                 return
-
-#             for i in candidates:
-#                 if sum(cur) + i <= target:
-#                     cur.append(candidates[l])
-#                     dfs(candidates, target, l, cur, ans)
-#                 dfs(candidates, target, l + 1, cur, ans)
-
-#         dfs(candidates, target, 0, cur, ans)
-#         return ans
-
-
+# @lc code=start
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         def dfs(candidates, target, s, cur, ans):
@@ -36,6 +16,7 @@ class Solution:
                     return
                 # remove duplicate number
                 # i>s,判断不是第一个元素,同一层不能继续使用,比如有两个1,对于当前这层递归循环,只能使用一个1
+                # 重点注意i和s分别控制的关系
                 if i > s and candidates[i] == candidates[i - 1]:
                     continue
                 cur.append(candidates[i])
@@ -48,6 +29,5 @@ class Solution:
         return ans
 
 
-s = Solution()
-print(s.combinationSum2([1, 1, 2, 5, 6, 7], 8))
+# @lc code=end
 
