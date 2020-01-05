419103ae75e3b742697bd3fd5653ff605745fe9b
debian <1@1.com>
2020-01-05 20:25:41 +0800

    46  permutation refine

--- 46.permutations.py
+++ 46.permutations.py
@@ -8,19 +8,20 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         ans = []
+        n = len(nums)
 
-        def dfs(d, n, used, cur):
-            if d == n:
+        def dfs(used, cur):
+            if len(cur) == n:
                 ans.append(cur[:])
                 return
-            for i in range(len(nums)):
+            for i in range(n):
                 if used & 1 << i:
                     continue
                 cur.append(nums[i])
-                dfs(d + 1, n, used | 1 << i, cur)
+                dfs(used | 1 << i, cur)
                 cur.pop()
 
-        dfs(0, len(nums), 0, [])
+        dfs(0, [])
         return ans
 
 
