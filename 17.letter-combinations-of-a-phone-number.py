5e5fe17249a014d27b1f17116e8cc37e14d67e01
debian <1@1.com>
2020-01-05 19:56:39 +0800

    17 letter refine

--- 17.letter-combinations-of-a-phone-number.py
+++ 17.letter-combinations-of-a-phone-number.py
@@ -40,18 +40,21 @@ class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         if not digits:
             return []
-
-        def dfs(cur, l):
-            if l == len(digits):
-                ans.append(cur)
-                return
-            for c in d[int(digits[l])]:
-                tmp = cur + c
-                dfs(tmp, l + 1)
-
         d = ["", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"]
+
         ans = []
-        dfs("", 0)
+        n = len(digits)
+
+        def dfs(s, cur):
+            if len(cur) == n:
+                ans.append(cur[:])
+                return
+            for c in d[int(digits[s])]:
+                cur += c
+                dfs(s + 1, cur)
+                cur = cur[:-1]
+
+        dfs(0, "")
         return ans
 
 
