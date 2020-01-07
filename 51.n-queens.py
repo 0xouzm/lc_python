2b842ebe4fa2c9de0155593448f9ce9c46f9f18e
ouzm <wjj315315@gmail.com>
2020-01-07 15:19:22 +0800

    n queens refine

--- 51.n-queens.py
+++ 51.n-queens.py
@@ -9,14 +9,18 @@ class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         ans = []
 
-        def dfs(row, cur):
-            if row == n:
-                ans.append(cur[:])
+        def dfs(queens, xy_sum, xy_dif):
+            p = len(queens)
+            if p == n:
+                ans.append(queens)
                 return
 
-            for i in range(n):
-                pass
+            for q in range(n):
+                if q not in queens and p + q not in xy_sum and p - q not in xy_dif:
+                    dfs(queens + [q], xy_sum + [p + q], xy_dif + [p - q])
 
+        dfs([], [], [])
+
+        return [['.' * i + 'Q' + '.' * (n - i - 1) for i in s] for s in ans]
 
 # @lc code=end
-
