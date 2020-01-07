a9b18b2fdf7d91ad8efa65a28c6f200865a4245c
ouzm <wjj315315@gmail.com>
2020-01-07 16:22:01 +0800

    word search

---
+++ 79.word-search.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=79 lang=python3
+#
+# [79] Word Search
+#
+
+# @lc code=start
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        def dfs(cur)
+        
+# @lc code=end
+
