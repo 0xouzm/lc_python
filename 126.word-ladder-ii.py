3bcc9e5fc1be370ff2dbcf953152b19b04834643
ouzm <wjj315315@gmail.com>
2020-01-10 16:36:22 +0800

    126 refine

--- 126.word-ladder-ii.py
+++ 126.word-ladder-ii.py
@@ -16,111 +16,107 @@
 # Given two words (beginWord and endWord), and a dictionary's word list, find
 # all shortest transformation sequence(s) from beginWord to endWord, such
 # that:
-# 
-# 
+#
+#
 # Only one letter can be changed at a time
 # Each transformed word must exist in the word list. Note that beginWord is not
 # a transformed word.
-# 
-# 
+#
+#
 # Note:
-# 
-# 
+#
+#
 # Return an empty list if there is no such transformation sequence.
 # All words have the same length.
 # All words contain only lowercase alphabetic characters.
 # You may assume no duplicates in the word list.
 # You may assume beginWord and endWord are non-empty and are not the same.
-# 
-# 
+#
+#
 # Example 1:
-# 
-# 
+#
+#
 # Input:
 # beginWord = "hit",
 # endWord = "cog",
 # wordList = ["hot","dot","dog","lot","log","cog"]
-# 
+#
 # Output:
 # [
 # ⁠ ["hit","hot","dot","dog","cog"],
 # ["hit","hot","lot","log","cog"]
 # ]
-# 
-# 
+#
+#
 # Example 2:
-# 
-# 
+#
+#
 # Input:
 # beginWord = "hit"
 # endWord = "cog"
 # wordList = ["hot","dot","dog","lot","log"]
-# 
+#
 # Output: []
-# 
+#
 # Explanation: The endWord "cog" is not in wordList, therefore no possible
 # transformation.
-# 
-# 
-# 
-# 
-# 
+#
+#
+#
+#
+#
 #
 
 # @lc code=start
 import string
 from collections import defaultdict
+
+
 class Solution:
-    def findLadders(self, beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
-        ans = defaultdict(list)
+    def findLadders(
+        self, beginWord: str, endWord: str, wordList: List[str]
+    ) -> List[List[str]]:
         wordDict = set(wordList)
-        wordDict.add(beginWord)
         if endWord not in wordDict:
             return []
-        s = {beginWord}
+        wordDict.add(beginWord)
+
+        children = defaultdict(list)
+        p = set([beginWord])
         l = len(beginWord)
+        ans = []
         found = False
 
-        while s and not found:
-            for w in s:
+        while p and not found:
+            q = set()
+            for w in p:
                 wordDict.remove(w)
-            tmp_s = set()
-            for w in s:
+            for w in p:
                 for i in range(l):
                     for c in string.ascii_lowercase:
-                        tmp = w[:i] + c + w[i+1:]
+                        tmp = w[:i] + c + w[i + 1 :]
                         if tmp == endWord:
                             found = True
-                            ans[endWord].append(w)
-                            continue
-                        if tmp not in wordDict:
-                            continue
-                        tmp_s.add(tmp)
-                        ans[tmp].append(w)
-            s = tmp_s
+                            children[endWord].append(w)
+                        else:
+                            if tmp not in wordDict:
+                                continue
+                            q.add(tmp)
+                            children[tmp].append(w)
+            p = q
 
-        def dfs(k,cur):
-            if ans[k][0] == beginWord:
-                res.append(cur[:]+[beginWord])
+        def dfs(word, endWord, cur, children, ans):
+            if word == endWord:
+                ans.append(cur[::-1])
                 return
-            for i in range(len(ans[k])):
-                if i > 0 and ans[k][i] == ans[k][i-1]:
-                    continue
-                cur.append(ans[k][i])
-                dfs(ans[k][i],cur)
+            for w in children[word]:
+                cur.append(w)
+                dfs(w, endWord, cur, children, ans)
                 cur.pop()
-        if not ans:
-            return []
-        res = []
-        dfs(endWord,[endWord])
-        for i in res:
-            i.reverse()
-        return res
-            
+
+        dfs(endWord, beginWord, [endWord], children, ans)
+        return ans
 
 
-                        
-                    
-        
 # @lc code=end
 
