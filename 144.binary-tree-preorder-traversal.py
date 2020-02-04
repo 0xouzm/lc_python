016a6080b56340e938a4e8b1f4bd66a812ebf36b
ouzm <wjj315315@gmail.com>
2020-02-04 15:22:45 +0800

    tree travel

---
+++ 144.binary-tree-preorder-traversal.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode id=144 lang=python3
+#
+# [144] Binary Tree Preorder Traversal
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        res, stack = [], [root]
+        while stack:
+            tmp = stack.pop()
+            if tmp:
+                res.append(tmp.val)
+                stack.append(tmp.right)
+                stack.append(tmp.left)
+        return res
+        
+# @lc code=end
+
---
+++ 93.restore-ip-addresses.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=93 lang=python3
+#
+# [93] Restore IP Addresses
+#
+
+# @lc code=start
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        
+        
+# @lc code=end
+
