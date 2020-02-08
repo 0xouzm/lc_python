3da5f3f0be212f19487441a224ed35966872216e
ouzm <wjj315315@gmail.com>
2020-02-08 11:17:23 +0800

    edit distance refine

--- 72.edit-distance.py
+++ 72.edit-distance.py
@@ -7,7 +7,7 @@
 # @lc code=start
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        # dp 含义：word1[i] 和 word2[j]之间的最小编辑距离
+        # dp 含义：word1[0...i] 和 word2[0...j]之间的最小编辑距离
         m = len(word1)
         n = len(word2)
         table = [[0] * (n + 1) for _ in range(m + 1)]
@@ -15,15 +15,18 @@ class Solution:
             table[i][0] = i
         for j in range(1, n + 1):
             table[0][j] = j
-        print(table)
-        for i in range(1,m+1):
-            for j in range(1,n+1):
+        # 这里注意range 范围
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
                 if word1[i - 1] == word2[j - 1]:
                     table[i][j] = table[i - 1][j - 1]
                 else:
                     table[i][j] = min(
+                        # delete
                         table[i - 1][j] + 1,
+                        # insert
                         table[i][j - 1] + 1,
+                        # replace
                         table[i - 1][j - 1] + 1,
                     )
 
