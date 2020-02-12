34c86912b6b218e7b3b4ac4af9ba01112554d3df
ouzm <wjj315315@gmail.com>
2020-02-12 21:48:22 +0800

    94

---
+++ 94.binary-tree-inorder-traversal.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=94 lang=python3
+#
+# [94] Binary Tree Inorder Traversal
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        
+# @lc code=end
+
