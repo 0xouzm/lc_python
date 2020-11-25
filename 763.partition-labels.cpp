d6bd493406007a100d8d1cbdaef9eafbff34853d
ouzm <wjj315315@gmail.com>
2020-11-25 15:15:18 +0800

    763 review

--- 763.partition-labels.cpp
+++ 763.partition-labels.cpp
@@ -8,22 +8,22 @@
 class Solution {
 public:
     vector<int> partitionLabels(string S) {
+        vector<int> res;
         int last[26];
-        int length = S.size();
-        for(int i = 0; i < length; ++ i){
+
+        for (int i = 0; i < S.size(); i++) {
             last[S[i] - 'a'] = i;
         }
-        vector<int> res;
-        int start = 0, end = 0;
-        for (int i = 0; i < length; ++i){
-            end = max(end , last[S[i] - 'a']);
-            if(i == end){
-                res.push_back(end - start + 1);
-                start = i + 1;
+
+        int a = 0, b = 0;
+        for (int i = 0; i < S.size(); i++) {
+            b = max(b, last[S[i] - 'a']);
+            if (b == i) {
+                res.push_back(b - a + 1);
+                a = i + 1;
             }
         }
         return res;
     }
 };
 // @lc code=end
-
