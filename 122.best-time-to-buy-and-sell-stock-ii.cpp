f449383bb4b9b718a3b1ef57600801a8096aadb5
ouzm <wjj315315@gmail.com>
2020-11-25 15:44:45 +0800

    122 review

--- 122.best-time-to-buy-and-sell-stock-ii.cpp
+++ 122.best-time-to-buy-and-sell-stock-ii.cpp
@@ -9,12 +9,11 @@ class Solution {
 public:
     int maxProfit(vector<int> &prices) {
         int profit = 0;
-        int len = prices.size();
-        for (int i = 0; i < len - 1; i++) {
-            profit += max(prices[i + 1] - prices[i], 0);
+
+        for (int i = 1; i < prices.size(); i++) {
+            profit += max(0, prices[i] - prices[i - 1]);
         }
         return profit;
     }
 };
 // @lc code=end
-
